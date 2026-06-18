5fb2bbea3504aaefa2687c54132807bd38159d5f
Yun Chen <mail@yunchen.dev>
2026-06-18 18:42:15 -0400

    Add: counting-bits - submission-2

---
+++ Data Structures & Algorithms/counting-bits/submission-2.py
@@ -0,0 +1,9 @@
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        ans = [0] * (n + 1)
+
+        for x in range(1, n + 1):
+            print(x & (x - 1) + 1)
+            ans[x] = ans[x & (x - 1)] + 1
+
+        return ans
